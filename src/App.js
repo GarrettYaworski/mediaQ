eaf7e50ef677f69f51a528b2574c8d0e2981f89c
Garrett Yaworski <garrett@Garretts-MacBook-Pro.local>
2018-09-03 15:55:56 -0500

    basic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='navbar'>
+          <p className='header-text'> Start Bootstrap</p>
+          <div className='navlinks'>
+            <div className='link'>SERVICES</div>
+            <div className='link'>PORTFOLIO</div>
+            <div className='link'>ABOUT</div>
+            <div className='link'>TEAM</div>
+            <div className='link'>CONTACT</div>
+            <button class='toggle-button'>MENU&#9776;</button>
+          </div>
+        </div>
+        <div className='middle'>
+          <p className='welcome-text'>Welcome To Our Studio!</p>
+          <p className= 'meet-text'>IT'S NICE TO MEET YOU</p>
+          <button>TELL ME MORE</button>
+        </div>
       </div>
     );
   }
